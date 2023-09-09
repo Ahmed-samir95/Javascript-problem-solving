7dfa74d55d2a33c1b690bc9eb289c399b48cff16
ahmed-samir <ahmedsamirr126@gmail.com>
2023-09-09 14:23:57 +0200

    Create a Function that performs various arithmetic operations

--- problem 4/main.js
+++ problem 4/main.js
@@ -12,3 +12,34 @@ function sayHello(theName, theGender) {
 sayHello("Osama", "Male"); // "Hello Mr Osama"
 sayHello("Eman", "Female"); // "Hello Miss Eman"
 sayHello("Sameh"); // "Hello Sameh"
+// --------------------------------------------------------------------------------------
+function calculate(firstNum, secondNum, operation) {
+    if (operation === "add") {
+      console.log(firstNum + secondNum);
+    } if (operation === "subtract") {
+      console.log(firstNum - secondNum);
+    } if (operation === "multiply") {
+      console.log(firstNum * secondNum);
+    } else {
+      console.log("Second Number Not Found");
+    }
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, 'add'); // 50
+calculate(20, 30, 'subtract'); // -10
+calculate(20, 30, 'multiply'); // 600
+
+
+
+
+
+
+
+
+
+
+
+
