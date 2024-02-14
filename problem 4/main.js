ae8802448bf7b5e9520318287346fe689447e8bb
ahmed-samir <ahmedsamirr126@gmail.com>
2024-02-14 16:24:49 +0200

    In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?

--- problem 4/main.js
+++ problem 4/main.js
@@ -81,8 +81,10 @@ function solution(str){
   return str.split("").reverse().join("");
 }
 console.log(solution("ahmed"));
-
-
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+function makeNegative(num) {
+  return num < 0 ? num : -num
+}
 
 
 
