73c0e02a48fdc7ff8a17b3fe667c1984d7c2ad79
ahmed-samir <ahmedsamirr126@gmail.com>
2024-02-19 15:27:11 +0200

    You get an array of numbers, return the sum of all of the positives ones.

--- problem 4/main.js
+++ problem 4/main.js
@@ -89,6 +89,30 @@ function makeNegative(num) {
 function boolToWord( bool ){
   return bool === true ? "Yes" : "No"
 }
+// You get an array of numbers, return the sum of all of the positives ones.
+function positiveSum(arr) {
+  let res = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      res += arr[i]
+    }
+  }
+  return res
+}
+
+console.log(positiveSum([1, -4, 7, 12]));
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
