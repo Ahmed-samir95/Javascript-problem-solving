0c4016cd0681656e9d313515e2fb23cf067fba11
ahmed-samir <ahmedsamirr126@gmail.com>
2024-02-21 15:54:43 +0200

    Complete the square sum function so that it squares each number passed into it and then sums the results together.

--- problem 4/main.js
+++ problem 4/main.js
@@ -118,7 +118,15 @@ function removeChar(str){
 
  };
 console.log(removeChar("ahmed"));
-
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+function squareSum(numbers){
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i] ** 2; 
+  }
+  return sum;
+}
+console.log(squareSum([1, 2, 2]));
 
 
 
